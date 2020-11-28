3b6574396fd25ed666b9927b5892da41db02da8b
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-11-28 14:11:06 +0200

    Create module

--- configs/base.js
+++ configs/base.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-process-env */
 module.exports = {
-	packageName: 'app-template',
-	description: 'App Template',
+	packageName: 'browser-debug',
+	description: 'Browser Debug',
 	tests      : {
 		intern: {
 
